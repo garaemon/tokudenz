26611a7ecee268cd6684eca28ab0a013cbb83b7a
garaemon <garaemon@gmail.com>
2012-05-26 02:10:40 +0900

    busy loop

--- tokuden.js
+++ tokuden.js
@@ -13,13 +13,18 @@ $(function() {
       var leftSecs = leftMilSecs / 1000.0;
       if (leftSecs < 0)
         location.href = "tel:" + tel;
-      else
-        setTimeout(tick, LOOP_MILSEC);
-      $(".inputs").html("<h3 class=\"resttime\">残り"
-                        + " <span class=\"restsec\">" + leftSecs
-                        + "秒!</span></h3>"
-                        + "<h3>" + tel + "に電話します</h3>");
+      else {
+        //setTimeout(tick, LOOP_MILSEC);
+        $(".inputs").html("<h3 class=\"resttime\">残り"
+                          + " <span class=\"restsec\">" + leftSecs
+                          + "秒!</span></h3>"
+                          + "<h3>" + tel + "に電話します</h3>");
+        tick();
+      }
+
+      
     };
-    setTimeout(tick, LOOP_MILSEC);
+    //setTimeout(tick, LOOP_MILSEC);
+    tick()
   });
 });
